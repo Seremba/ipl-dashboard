c535025ed99404e006fc4bd2fcd8f038807d97eb
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-02-28 12:21:23 +0000

    create Matches page

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,16 @@
+import {React, useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom';
+import { MatchDetailsCard } from '../components/MatchDetailsCard';
+import { MatchSmallCard } from '../components/MatchSmallCard';
+
+export const MatchPage = () => {
+
+  return (
+    <div className="MatchPage">   
+         <h1>Match Page</h1>      
+    </div>
+  );
+}
+ 
+
+
