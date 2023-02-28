068b79ba3204aa51401e293b01e0e57f863e9b33
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-02-28 12:21:50 +0000

    add a route to match page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom"
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/teams/:teamName" element={<TeamPage />} />
+        <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
       </Routes>
     </div>
   );
