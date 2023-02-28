f946bd1d61194a602d50fc06dbd5d6f08dbd81b5
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-02-28 14:25:08 +0000

    add matches end point

--- src/main/java/com/seremba/ipldashboard/controller/TeamController.java
+++ src/main/java/com/seremba/ipldashboard/controller/TeamController.java
@@ -1,10 +1,15 @@
 package com.seremba.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seremba.ipldashboard.model.Match;
 import com.seremba.ipldashboard.model.Team;
 import com.seremba.ipldashboard.repository.MatchRepository;
 import com.seremba.ipldashboard.repository.TeamRepository;
@@ -27,5 +32,16 @@ public class TeamController {
         team.setMatches(this.matchRepository.findLatestMatchesbyTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(
+            teamName,
+            startDate,
+            endDate
+            );
     }
 }
