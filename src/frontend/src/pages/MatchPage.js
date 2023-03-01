ad4b06c0777e11467ec8d327f74842ef40c005d6
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-03-01 09:28:42 +0000

    fetch matches and display them on MatchPage

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,13 +1,28 @@
 import {React, useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom';
 import { MatchDetailsCard } from '../components/MatchDetailsCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
+
 
 export const MatchPage = () => {
+ const [matches, setMatches] = useState([]);
+ const {teamName, year} = useParams();
+ 
+ useEffect(
+  () => {
+     const fetchMatches = async () => {
+       const response = await fetch(`https://seremba-scaling-system-g75v6gvvjj4hp4x9-8080.preview.app.github.dev/team/${teamName}/matches?year=${year}`);
+       const data = await response.json();
+       console.log(data);
+       setMatches(data);
+     }
+     fetchMatches();
 
+    }, [teamName, year]);
   return (
     <div className="MatchPage">   
-         <h1>Match Page</h1>      
+         <h1>Match Page</h1>   
+
+         {matches.map(match => (<MatchDetailsCard teamName={teamName} key={match.id} match={match}/>))}   
     </div>
   );
 }
