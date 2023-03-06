5c43477d87d8d58b48aa5899965d0e31a952eb46
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-03-06 23:33:19 +0000

    add additional details to the match

--- src/frontend/src/components/MatchDetailsCard.js
+++ src/frontend/src/components/MatchDetailsCard.js
@@ -7,12 +7,27 @@ export const MatchDetailsCard = ({match, teamName}) => {
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`
   return (
-    <div className="MatchDetailsCard">   
-         <span className='vs'>vs</span>
+    <div className="MatchDetailsCard"> 
+      <div>
+      <span className='vs'>vs</span>
          <h1> <Link to={otherTeamRoute}>{otherTeam}</Link> </h1>
          <h2 className='match-date'>{match.date}</h2>
          <h3 className='match-venue'>at {match.venue}</h3>
          <h3 className='match-result'> {match.matchWinner} won by {match.resultMargin} {match.result}</h3>
+      </div>  
+
+      <div className='additional-details'>
+         <h3>First Innings</h3>
+         <p>{match.team1}</p>
+         <h3>Second Innings</h3>
+         <p>{match.team2}</p>
+
+         <h3>Man of the match</h3>
+         <p>{match.playerOfMatch}</p>
+         <h3>Unmpires</h3>
+         <p>{match.umpire1}, {match.umpire2}</p>
+      </div>
+
     </div>
   );
 }
