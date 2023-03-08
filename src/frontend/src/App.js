9eed67a966b22c7cf7e2ea7f7c12fdd360411877
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-03-08 00:22:46 +0000

    add a home route

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.scss';
 import { Route, Routes } from "react-router-dom"
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePages';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/teams/:teamName" element={<TeamPage />} />
         <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
       </Routes>
